14440e0703ba79d998577871e7685b9bca9b5a78
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-06-11 11:00:53 +0200

    Call ReallocateCache() on each Flush()
    This frees up all associated memory with the map, not only the nodes.
    This is necessary in preparation for using the PoolAllocator for
    CCoinsMap, which does not actually free any memory on clear().

--- src/coins.cpp
+++ src/coins.cpp
@@ -235,6 +235,7 @@ bool CCoinsViewCache::BatchWrite(CCoinsMap &mapCoins, const uint256 &hashBlockIn
 bool CCoinsViewCache::Flush() {
     bool fOk = base->BatchWrite(cacheCoins, hashBlock);
     cacheCoins.clear();
+    ReallocateCache();
     cachedCoinsUsage = 0;
     return fOk;
 }
