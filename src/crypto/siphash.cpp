252f8e35ce7e35558920573dad28d9f3dc117c9c
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2025-03-28 22:21:11 +0100

    hash: format SIPROUND

--- src/crypto/siphash.cpp
+++ src/crypto/siphash.cpp
@@ -6,14 +6,23 @@
 
 #include <bit>
 
-#define SIPROUND do { \
-    v0 += v1; v1 = std::rotl(v1, 13); v1 ^= v0; \
-    v0 = std::rotl(v0, 32); \
-    v2 += v3; v3 = std::rotl(v3, 16); v3 ^= v2; \
-    v0 += v3; v3 = std::rotl(v3, 21); v3 ^= v0; \
-    v2 += v1; v1 = std::rotl(v1, 17); v1 ^= v2; \
-    v2 = std::rotl(v2, 32); \
-} while (0)
+#define SIPROUND                \
+    do {                        \
+        v0 += v1;               \
+        v1 = std::rotl(v1, 13); \
+        v1 ^= v0;               \
+        v0 = std::rotl(v0, 32); \
+        v2 += v3;               \
+        v3 = std::rotl(v3, 16); \
+        v3 ^= v2;               \
+        v0 += v3;               \
+        v3 = std::rotl(v3, 21); \
+        v3 ^= v0;               \
+        v2 += v1;               \
+        v1 = std::rotl(v1, 17); \
+        v1 ^= v2;               \
+        v2 = std::rotl(v2, 32); \
+    } while (0)
 
 CSipHasher::CSipHasher(uint64_t k0, uint64_t k1)
 {
