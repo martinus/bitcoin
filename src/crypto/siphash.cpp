2f53878f1a4e4e369244da6393aa40e7842b29c7
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2025-03-29 08:54:58 +0100

    reorder siphash

--- src/crypto/siphash.cpp
+++ src/crypto/siphash.cpp
@@ -9,18 +9,18 @@
 #define SIPROUND                \
     do {                        \
         v0 += v1;               \
-        v1 = std::rotl(v1, 13); \
-        v1 ^= v0;               \
-        v0 = std::rotl(v0, 32); \
         v2 += v3;               \
+        v1 = std::rotl(v1, 13); \
         v3 = std::rotl(v3, 16); \
+        v1 ^= v0;               \
         v3 ^= v2;               \
-        v0 += v3;               \
-        v3 = std::rotl(v3, 21); \
-        v3 ^= v0;               \
+        v0 = std::rotl(v0, 32); \
         v2 += v1;               \
+        v0 += v3;               \
         v1 = std::rotl(v1, 17); \
+        v3 = std::rotl(v3, 21); \
         v1 ^= v2;               \
+        v3 ^= v0;               \
         v2 = std::rotl(v2, 32); \
     } while (0)
 
